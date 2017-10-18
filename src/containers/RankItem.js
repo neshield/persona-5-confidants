95341f52790a1c3ae973862f3ea3a6a11f3adcd1
neshield <nefshields@gmail.com>
2017-10-17 21:52:30 -0700

    Choices get rendered with no styling

--- src/containers/RankItem.js
+++ src/containers/RankItem.js
@@ -2,11 +2,49 @@ import React, { Component } from 'react'
 import '../index.css'
 
 class RankItem extends Component {
+  // renderChoices(choices) {
+  //   return (
+  //     {choices ? (
+  //       <span> found choices</span>
+  //       ) : null}
+
+  //   )
+  // }
+
+  renderChoice (choice) {
+    return (
+      <li>
+        <span>{choice}</span>
+      </li>
+    )
+  }
+
+  renderChoices (choices) {
+    return (
+      <div>
+        {choices ? (
+          <ul>
+            {choices.map((choice) => {
+              return this.renderChoice(choice)
+            })}
+          </ul>
+        ) : null}
+      </div>
+    )
+  }
+
+        //   {this.props.rank.choices ? (
+        //     <span> found choices</span>
+        // ) : null}
+
   render () {
     // TODO: style text for abilities
     // TODO: choices
     return (
       <div className='RankItem'>
+        <div className='Choices'>
+          {this.renderChoices(this.props.rank.choices)}
+        </div>
         <span><strong>Rank {this.props.rank.rank}</strong><br />{this.props.rank.text}</span>
       </div>
     )
