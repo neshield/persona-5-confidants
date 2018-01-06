62fdfe281089f7b6bedc6612022293ccd2e9b70a
neshield <nefshields@gmail.com>
2018-01-06 15:40:00 -0800

    Add hardcoregamer source

--- src/App.js
+++ src/App.js
@@ -71,6 +71,9 @@ class App extends Component {
           <br />
           <a target='_blank' rel='noopener noreferrer'
             href='https://www.gamefaqs.com/ps4/835628-persona-5/faqs/73868'>Confidant Guide by JustChillin</a>
+          <br />
+          <a target='_blank' rel='noopener noreferrer'
+            href='https://www.hardcoregamer.com/2017/04/04/persona-5-confidant-guide/252919/'>Persona 5 Confidant Guide | Hardcore Gamer</a>
         </footer>
       </div>
     )
