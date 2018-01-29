63c674d12079ba1ef13448a833b961f9d0af6d37
neshield <nefshields@gmail.com>
2018-01-28 21:31:21 -0800

    Finish Sojiro and Yusuke. 3 left!

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      confidant: 'shinya',
+      confidant: 'ann',
       labels: confidantLabels
     }
   }
